4426257549002480b089f659a5475fa497ca57af
Blingboy740 <44981134+Blingboy740@users.noreply.github.com>
2018-12-10 19:53:36 +0000

    Update MainCodeDay2.py

--- Day 2/MainCodeDay2.py
+++ Day 2/MainCodeDay2.py
@@ -43,6 +43,9 @@ print("")
 finalValue = rep2 * rep3
 print("TOTAL = ",finalValue)
 
+#Code By Bogdan Cuciureanu  # NOTE when using this code don't forget to use your own input
+#Any suggestions on how to make it better will not be ignored, feel free to submit your code :)
+
 
 
         

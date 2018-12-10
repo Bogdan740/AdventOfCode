f8bf72f0f16619c2b6ad4026aceadfa86fbeac74
Blingboy740 <44981134+Blingboy740@users.noreply.github.com>
2018-12-10 19:50:01 +0000

    Update MainCodeDay1.py

--- Day 1/MainCodeDay1.py
+++ Day 1/MainCodeDay1.py
@@ -1,4 +1,4 @@
-cinput = open("CodeInputDay1.txt", "r")
+cinput = open("CodeInputDay1.txt", "r") #Code By Bogdan Cuciureanu  # NOTE when using this code don't forget to use your own input
 a = cinput.read()
 inputList = a.split("\n")
 cinput.close()

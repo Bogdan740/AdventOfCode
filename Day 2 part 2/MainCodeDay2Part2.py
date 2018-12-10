b9eef426a406b18c9961c20408702a3d6d31ccb4
Blingboy740 <44981134+Blingboy740@users.noreply.github.com>
2018-12-10 19:55:34 +0000

    Update MainCodeDay2Part2.py

--- Day 2 part 2/MainCodeDay2Part2.py
+++ Day 2 part 2/MainCodeDay2Part2.py
@@ -34,3 +34,6 @@ for i in inputList:
             break
         else:
             pass
+        
+#Code By Bogdan Cuciureanu  # NOTE when using this code don't forget to use your own input
+#Any suggestions on how to make it better will not be ignored, feel free to submit your code :)

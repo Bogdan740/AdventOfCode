0602d74b8906e6a9a3481dacd2ca2cbde866d77e
Blingboy740 <44981134+Blingboy740@users.noreply.github.com>
2018-12-10 19:52:14 +0000

    Update MainCodeDay2Part2.py

--- Day 1 part 2/MainCodeDay2Part2.py
+++ Day 1 part 2/MainCodeDay2Part2.py
@@ -21,6 +21,8 @@ while True:
     position = (position + 1) % len(input_lines)
 
     
+#Code By Bogdan Cuciureanu  # NOTE when using this code don't forget to use your own input
+#Any suggestions on how to make it better will not be ignored, feel free to submit your code :)
         
         
     

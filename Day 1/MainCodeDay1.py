6e37e40ee8dfa6e877b6a0c40518597eb0108dc2
Blingboy740 <44981134+Blingboy740@users.noreply.github.com>
2018-12-10 19:51:16 +0000

    Update MainCodeDay1.py

--- Day 1/MainCodeDay1.py
+++ Day 1/MainCodeDay1.py
@@ -1,5 +1,5 @@
-cinput = open("CodeInputDay1.txt", "r") #Code By Bogdan Cuciureanu  # NOTE when using this code don't forget to use your own input
-a = cinput.read()
+cinput = open("CodeInputDay1.txt", "r")
+a = cinput.read()                                                 
 inputList = a.split("\n")
 cinput.close()
 
@@ -8,7 +8,8 @@ wholeSum = sum(inputList, 0)
 
 print(wholeSum)
 
-
+ #Code By Bogdan Cuciureanu  # NOTE when using this code don't forget to use your own input
+ #Any suggestions on how to make it better will not be ignored, feel free to submit your code :)
 
 
     

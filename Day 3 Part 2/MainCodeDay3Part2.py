f24a788a4624ff4639d4942ddb5fd2c82284f91b
Blingboy740 <44981134+Blingboy740@users.noreply.github.com>
2018-12-11 19:54:55 +0000

    Update MainCodeDay3Part2.py

--- Day 3 Part 2/MainCodeDay3Part2.py
+++ Day 3 Part 2/MainCodeDay3Part2.py
@@ -74,6 +74,13 @@ for i in range(len(inputList)):
                                 tester +=1
                 if tester == (smm * smm1) and tester > 0:
                                 print(inputList[i])
+                              
+                        
+                        
+                        
+                        
+  #Code By Bogdan Cuciureanu  # NOTE when using this code don't forget to use your own input
+ #Any suggestions on how to make it better will not be ignored, feel free to submit your code :)
                                 
                 
 

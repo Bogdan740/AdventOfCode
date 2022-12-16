375ea9b9f7cea350372bbd52f8aa7bdc0e89bc84
Mihai-Bogdan Cuciureanu <bogdancuciureanup@gmail.com>
2022-12-16 00:39:35 +0000

    Day 10 commeting

--- 2022/d10/p2.py
+++ 2022/d10/p2.py
@@ -8,15 +8,14 @@ parsed = [i.split(" ") for i in inp.split("\n")]
 def draw(cycle,x,screen):
   screen[cycle-1] = '#' if x-1 <= (cycle -1)%40  <= x+1 else '.'
 
-
 screen = ['' for _ in range(240)]
 x = 1
 cycle = 0
 for cmd in parsed:
-  if(cmd[0] == "noop"):
+  if(cmd[0] == "noop"): # 'noop' command
     cycle +=1
     draw(cycle,x,screen)
-  else:
+  else: # 'addx' commadn
     for _ in range(2):
       cycle +=1
       draw(cycle,x,screen)

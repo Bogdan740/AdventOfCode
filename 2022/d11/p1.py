95902a3606036e61ff29ef70407e3dae3fd8c719
Mihai-Bogdan Cuciureanu <bogdancuciureanup@gmail.com>
2022-12-17 12:23:22 +0000

    day 11 done

--- 2022/d11/p1.py
+++ 2022/d11/p1.py
@@ -1,28 +1,49 @@
-from math import sqrt
+from math import floor
 
-f = open("input.txt", "r")
+f = open("sample.txt", "r")
 
 inp = f.read()
-parsed = [i.split(" ") for i in inp.split("\n")]
+parsed = inp.split("\n")
 
-x = 1
-cycle = 0
-signalStrength = 0
-for cmd in parsed:
-  if(cmd[0] == "noop"):
-    cycle +=1
-    if((cycle-20)%40 == 0 or cycle == 20):
-      signalStrength += cycle*x
-  else:
-    num = int(cmd[1])
-    cycle +=1
-    if((cycle-20)%40 == 0 or cycle == 20):
-      signalStrength += cycle*x
-    cycle +=1
-    if((cycle-20)%40 == 0 or cycle == 20):
-      signalStrength += cycle*x
-    x+=num
-    
-print(signalStrength)
-      
-    
+class Monkey:
+  def __init__(self,id, items, operation, test, ifTrue, ifFalse):
+    self.id = id
+    self.items = items
+    self.operation = operation
+    self.test = test
+    self.ifTrue = ifTrue
+    self.ifFalse = ifFalse
+    self.totalInspected = 0
+  def print(self):
+    print("ID : {}, items : {}, operation : {}, test : {}, ifTrue : {}, ifFalse {}".format(self.id,self.items,self.operation,self.test,self.ifTrue, self.ifFalse))
+monkeys = {}
+
+for i in range(0,len(parsed),7):
+  monkeyId= i//7 # Monkey identifier
+  startingItems = list(map(int,parsed[i+1].replace(",", "").split(" ")[4:])) # The items that the monkey starts with FORM : [Int]
+  operation = " ".join(parsed[i+2].split(" ")[5:]) # What to do with the stress level FORM : "[operator] [operand]"
+  test = int(parsed[i+3].split(" ")[-1]) # How to tell if you should throw current item (what to be divisble by) FORM : Int
+  trueAction = int(parsed[i+4].split(" ")[-1]) # Who to throw to if test passes FORM : Int
+  falseAction = int(parsed[i+5].split(" ")[-1]) # Who to throw to if test fails FORM : Int
+  
+  monkeys[monkeyId] = Monkey(monkeyId, startingItems, operation, test, trueAction, falseAction)
+
+def elapseRound(monkeys):
+  for _,monkey in monkeys.items():
+    for _,item in enumerate(monkey.items):
+      old = item
+      new = eval(monkey.operation)
+      newDiv3 = floor(new/3)
+      if(newDiv3 % monkey.test == 0):
+        monkeys[monkey.ifTrue].items.append(newDiv3)
+      else:
+        monkeys[monkey.ifFalse].items.append(newDiv3)
+    monkey.totalInspected = monkey.totalInspected + len(monkey.items)
+    monkey.items = []
+
+for i in range(20):
+  elapseRound(monkeys)
+
+totalInspectedArray = sorted([(monkey.totalInspected) for (_,monkey) in monkeys.items()])
+monkeyBusiness = totalInspectedArray[-1] * totalInspectedArray[-2]
+print(monkeyBusiness)
--- 2022/d11/p2.py
+++ 2022/d11/p2.py
@@ -1,28 +1,50 @@
-from math import sqrt
-
 f = open("input.txt", "r")
 
 inp = f.read()
-parsed = [i.split(" ") for i in inp.split("\n")]
+parsed = inp.split("\n")
 
-def draw(cycle,x,screen):
-  screen[cycle-1] = '#' if x-1 <= (cycle -1)%40  <= x+1 else '.'
-
-screen = ['' for _ in range(240)]
-x = 1
-cycle = 0
-for cmd in parsed:
-  if(cmd[0] == "noop"): # 'noop' command
-    cycle +=1
-    draw(cycle,x,screen)
-  else: # 'addx' commadn
-    for _ in range(2):
-      cycle +=1
-      draw(cycle,x,screen)
-    x+=int(cmd[1])
-
+class Monkey:
+  def __init__(self,id, items, operation, test, ifTrue, ifFalse):
+    self.id = id
+    self.things = items
+    self.operation = operation
+    self.test = test
+    self.ifTrue = ifTrue
+    self.ifFalse = ifFalse
+    self.totalInspected = 0
     
-for i in range(0,len(screen),40):
-  print("".join(screen[i:i+40]))
-      
-    
+factorsProduct = 1
+for i in range(0,len(parsed),7):
+  test = int(parsed[i+3].split(" ")[-1]) # How to tell if you should throw current item (what to be divisble by) FORM : Int
+  factorsProduct *= test
+  
+monkeys = {}
+for i in range(0,len(parsed),7):
+  monkeyId= i//7 # Monkey identifier
+  startingItems = list(map(int,parsed[i+1].replace(",", "").split(" ")[4:])) # The items that the monkey starts with FORM : [Int]
+  operation = " ".join(parsed[i+2].split(" ")[5:]) # What to do with the stress level FORM : "[operator] [operand]"
+  test = int(parsed[i+3].split(" ")[-1]) # How to tell if you should throw current item (what to be divisble by) FORM : Int
+  trueAction = int(parsed[i+4].split(" ")[-1]) # Who to throw to if test passes FORM : Int
+  falseAction = int(parsed[i+5].split(" ")[-1]) # Who to throw to if test fails FORM : Int
+  monkeys[monkeyId] = Monkey(monkeyId, startingItems, operation, test, trueAction, falseAction)
+
+
+def elapseRound(monkeys):
+  for _,monkey in monkeys.items():
+    for _,item in enumerate(monkey.things):
+      old = item
+      new = eval(monkey.operation + "%" + str(factorsProduct))
+      if(new % monkey.test == 0):
+        monkeys[monkey.ifTrue].things.append(new)
+      else:
+        monkeys[monkey.ifFalse].things.append(new)
+    monkey.totalInspected = monkey.totalInspected + len(monkey.things)
+    monkey.things = []
+
+for i in range(10000):
+  elapseRound(monkeys)
+
+totalInspectedArray = sorted([(monkey.totalInspected) for (_,monkey) in monkeys.items()])
+monkeyBusiness = totalInspectedArray[-1] * totalInspectedArray[-2]
+print(totalInspectedArray)
+print(monkeyBusiness)

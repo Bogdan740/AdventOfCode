eebe0e904c5d33f852e51b5341d216983ab366dd
Mihai-Bogdan Cuciureanu <bogdancuciureanup@gmail.com>
2022-12-20 00:09:18 +0000

    start of DP solution for day 16

--- 2022/d16 /p1.py
+++ 2022/d16 /p1.py
@@ -1,28 +1,47 @@
 f = open("sample.txt", "r")
-valves = {ord(line.split()[1][1])-65:(int(line.split()[4][5:].replace(";","")),[], list(map(lambda x:ord(x[0])-65,list(map(lambda x :x.replace(",",""),line.split()[9:]))))) for line in f.read().split("\n")}
-DP = [[ 0 for _ in range(31)] for _ in range(len(valves))]
+valves = {ord(line.split()[1][1])-65:(int(line.split()[4][5:].replace(";","")),list(map(lambda x:ord(x[0])-65,list(map(lambda x :x.replace(",",""),line.split()[9:]))))) for line in f.read().split("\n")}
 
-for v in valves:
-  for k in valves:
-    if(v in valves[k][2]):
-      valves[v][1].append(k)
+def distanceFromTo(fromV, toV, valves):
+  queue = [fromV]
+  dist = 0
+  while(len(queue) !=0):
+    for _ in range(len(queue)):
+      current = queue.pop(0)
+      if(current == toV):
+        return dist
+      for n in valves[current][1]:
+        queue.append(n)    
+    dist+=1
+
+closed = [valve for valve in valves]
+current = 0 # Set our position to AA
+time = 30
+totalP = 0
+while(time >= 0):
+  maxP = -float('inf')
+  chosen = None
+  timePenalty = 0
+  for i in closed:
+    dist = distanceFromTo(current,i,valves)
+    if(time - dist - 1 > 0):
+      pressureReleased = (time-dist-1) * valves[i][0]
+      if(pressureReleased > maxP):
+        maxP = pressureReleased
+        chosen = i
+        timePenalty = dist + 1
+  if(chosen == None):
+    break
+  time -=timePenalty
+  totalP += maxP
+  current = chosen
+  print(chosen)
+  closed.remove(chosen)
+
+print(totalP)
+  
     
-opened = {valve:False for valve in valves}
-
-for valve in range(len(DP)):
-  for minute in range(1,len(DP[0])):
-    comeFrom = valves[valve][1] # Valves where we could have come from
-    if(opened[valve]):
-      arr = [ DP[v][minute-1] for v in comeFrom]
-      DP[valve][minute] = max(arr)
-    else:
-      pressure = valves[valve][0]
-      arr = [ DP[v][minute-1] for v in comeFrom]
-      DP[valve][minute] = max(max(arr), (30-minute+1) * pressure)
-
-for i in DP:
-  print(i)
-      
-      
     
+  
+  
+  
     

6c2110463a3baaa83526ee1b6d6fe1dcc6729a9d
Mihai-Bogdan Cuciureanu <bogdancuciureanup@gmail.com>
2022-12-21 16:31:53 +0000

    *

--- 2022/d15/p2.py
+++ 2022/d15/p2.py
@@ -1,10 +1,9 @@
-f = open("input.txt", "r")
+f = open("sample.txt", "r")
 
 sensBeac = [ [(int(k[2][2:-1]),int(k[3][2:-1])), (int(k[-2][2:-1]),int(k[-1][2:]))] for k in [(line.split()) for line in f.read().split("\n")]]
 sensBeac = sorted([ ((sx,sy),(bx,by),abs(sx - bx) + abs(sy-by)) for (sx,sy),(bx,by) in sensBeac], key=lambda x:x[0] )
 
-mx = 4000000
-totalX = sum(range(1,mx+1))
+mx = 20
 for y in range(1,mx+1):
   x = 0
   for (sx,sy),(bx,by),distSB in sensBeac:

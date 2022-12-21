122a67fe86ce5469917bddcff0f218c733d2cf59
Mihai-Bogdan Cuciureanu <bogdancuciureanup@gmail.com>
2022-12-21 00:58:05 +0000

    Day 16 done

--- 2022/d16 /p2.py
+++ 2022/d16 /p2.py
@@ -19,10 +19,19 @@ for i in valves:
       for n in valves[current][1]:
         queue.append(n)    
     dist+=1
-
-open = 2**len(valves)-1
-DP = {}
-def recurse(valve, time):
+    
+def sublists(xs):
+  l = len(xs)
+  for i in range(1 << l):
+      incl, excl = [], []
+      for j in range(l):
+          if i & (1 << j):
+              incl.append(xs[j])
+          else:
+              excl.append(xs[j])
+      yield (incl,excl)
+  
+def recurse(valve, time, allowed):
   global open
   if(open == 0):
     return 0
@@ -30,21 +39,40 @@ def recurse(valve, time):
   if(c in DP):
     return DP[c]
   maxP = -float('inf')
-  for i in valves:
+  for i in allowed:
     if(valves[i][0] == 0):
       continue
-    isOpenIdentifier = 2**(valves[i][3])
+    isOpenIdentifier = 2**allowed[i]
     if((open & isOpenIdentifier) == 0):
       continue
     newTime = time-valves[valve][2][i]-1
     if(newTime > 0):
       pressureReleased = newTime * valves[i][0]
       open ^= isOpenIdentifier
-      pressure = pressureReleased + recurse(i,newTime)
+      pressure = pressureReleased + recurse(i,newTime,allowed)
       if(pressure > maxP):
         maxP = pressure
       open ^= isOpenIdentifier
   DP[c] = maxP if maxP != -float('inf') else 0
   return DP[c]
 
-print(recurse(start,30))
+splits = list(sublists([i for i in valves if valves[i][0]!=0]))
+splits = splits[:len(splits)//2]
+maxP = -float('inf')
+
+
+for i,(x,y) in enumerate(splits):
+  x+=[start];y+=[start]
+  p = 0
+  DP = {}
+  open = 2**(len(x)+1)-1
+  allowed1 = {val:i for i,val in enumerate(x) } 
+  p += recurse(start,26, allowed1)
+  DP = {}
+  open = 2**(len(y)+1)-1
+  allowed2 = {val:i for i,val in enumerate(y)}
+  p += recurse(start,26, allowed2)
+  if(p > maxP):
+    maxP = p
+
+print(maxP)

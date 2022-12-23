53695537b4fd888c179be9b14867e0d72969fca0
Mihai-Bogdan Cuciureanu <bogdancuciureanup@gmail.com>
2022-12-23 17:12:50 +0000

    d17 p1

--- 2022/d17/p1.py
+++ 2022/d17/p1.py
@@ -1,137 +1,83 @@
 import time
 
 f = open("sample.txt","r")
+jetPattern = list(map(lambda x:-1 if x=='<' else 1,f.read()))
 
-jetPattern = f.read()
-jet = 0
-# shapes = {0 :["####"], 1 : [".#.", "###", ".#."], 2 : ["..#","..#","###"], 3: ["#","#","#","#"], 4 :["##","##"] }
-chamber = [["." for _ in range(7)] for _ in range(4*2022)]
-
+drops =  2022
+#drops = 1000000000000
+toUse = int(drops*1.6)
+chamber = [[0 for _ in range(7)] for _ in range(toUse)]
 directions = {"<" : -1, ">" : 1}
-def nextJet():
-  global jet
-  global jetPattern
-  toReturn = jetPattern[jet]
-  jet = (jet + 1) % len(jetPattern)
-  return directions[toReturn]
+
+jet = 0
+
+
 def placeShape(chamber, shapeID, heightToDropFrom,x):
   if(shapeID == 0):  # TODO use case statement
-    chamber[heightToDropFrom][x] = "#"
-    chamber[heightToDropFrom][x+1] = "#"
-    chamber[heightToDropFrom][x+2] = "#"
-    chamber[heightToDropFrom][x+3] = "#"
-  if(shapeID == 1): 
-    chamber[heightToDropFrom+2][x+1] = "#"
-    chamber[heightToDropFrom+1][x] = "#"
-    chamber[heightToDropFrom+1][x+1] = "#"
-    chamber[heightToDropFrom+1][x+2] = "#"
-    chamber[heightToDropFrom][x+1] = "#"
-  if(shapeID == 2):  
-    chamber[heightToDropFrom][x] = "#"
-    chamber[heightToDropFrom][x+1] = "#"
-    chamber[heightToDropFrom][x+2] = "#"
-    chamber[heightToDropFrom+1][x+2] = "#"
-    chamber[heightToDropFrom+2][x+2]= "#"
-  if(shapeID == 3):  
-    chamber[heightToDropFrom+3][x] = "#"
-    chamber[heightToDropFrom+2][x] = "#"
-    chamber[heightToDropFrom+1][x] = "#"
-    chamber[heightToDropFrom][x] = "#"
-  if(shapeID == 4): 
-    chamber[heightToDropFrom+1][x] = "#"
-    chamber[heightToDropFrom+1][x+1] = "#"
-    chamber[heightToDropFrom][x] = "#"
-    chamber[heightToDropFrom][x+1] = "#"
-    
-def pushShape(chamber, shapeID,x,y,direction):
-  if(shapeID == 0):
-    for i in range(4):
-      chamber[y][x+i] = "."
-    for i in range(4):
-      chamber[y][x+i+direction] = "#"
-  elif(shapeID == 1):
-    chamber[y][x+1] = "."
-    chamber[y-1][x] = "."
-    chamber[y-1][x+1] = "."
-    chamber[y-1][x+2] = "."
-    chamber[y-2][x+1] = "."
-    
-    chamber[y][x+1+direction] = "#"
-    chamber[y-1][x+direction] = "#"
-    chamber[y-1][x+1+direction] = "#"
-    chamber[y-1][x+2+direction] = "#"
-    chamber[y-2][x+1+direction] = "#"
-  elif(shapeID == 2):
-    chamber[y][x] = "."
-    chamber[y][x+1] = "."
-    chamber[y][x+2] = "."
-    chamber[y+1][x+2] = "."
-    chamber[y+2][x+2]= "."
-    
-    chamber[y][x+direction] = "#"
-    chamber[y][x+1+direction] = "#"
-    chamber[y][x+2+direction] = "#"
-    chamber[y+1][x+2+direction] = "#"
-    chamber[y+2][x+2+direction]= "#"
-  elif(shapeID == 3):
-    chamber[y+3][x] = "."
-    chamber[y+2][x] = "."
-    chamber[y+1][x] = "."
-    chamber[y][x] = "."
-    
-    chamber[y+3][x+direction] = "#"
-    chamber[y+2][x+direction] = "#"
-    chamber[y+1][x+direction] = "#"
-    chamber[y][x+direction] = "#"
-  elif(shapeID == 4):
-    chamber[y+1][x] = "."
-    chamber[y+1][x+1] = "."
-    chamber[y][x] = "."
-    chamber[y][x+1] = "."
-    
-    chamber[y+1][x+direction] = "#"
-    chamber[y+1][x+1+direction] = "#"
-    chamber[y][x+direction] = "#"
-    chamber[y][x+1+direction] = "#"
-    
+    chamber[heightToDropFrom][x] = 1
+    chamber[heightToDropFrom][x+1] = 1
+    chamber[heightToDropFrom][x+2] = 1
+    chamber[heightToDropFrom][x+3] = 1
+  elif(shapeID == 1): 
+    chamber[heightToDropFrom+2][x+1] = 1
+    chamber[heightToDropFrom+1][x] = 1
+    chamber[heightToDropFrom+1][x+1] = 1
+    chamber[heightToDropFrom+1][x+2] = 1
+    chamber[heightToDropFrom][x+1] = 1
+  elif(shapeID == 2):  
+    chamber[heightToDropFrom][x] = 1
+    chamber[heightToDropFrom][x+1] = 1
+    chamber[heightToDropFrom][x+2] = 1
+    chamber[heightToDropFrom+1][x+2] = 1
+    chamber[heightToDropFrom+2][x+2]= 1
+  elif(shapeID == 3):  
+    chamber[heightToDropFrom+3][x] = 1
+    chamber[heightToDropFrom+2][x] = 1
+    chamber[heightToDropFrom+1][x] = 1
+    chamber[heightToDropFrom][x] = 1
+  else: 
+    chamber[heightToDropFrom+1][x] = 1
+    chamber[heightToDropFrom+1][x+1] = 1
+    chamber[heightToDropFrom][x] = 1
+    chamber[heightToDropFrom][x+1] = 1
+
+
+def pushShape(chamber, shapeID, sx, sy, pushK = None):
   
+  global jet
+  global jetPattern
+  push = jetPattern[jet]
+  jet = (jet + 1) % len(jetPattern)
 
-
-def pushTing(chamber, shapeID, sx, sy, pushK = None):
-  push = nextJet()
   if(pushK):
     push = pushK
   if(shapeID == 0):
     if(0 <= sx + push< 7 and ( 
-        (push == -1 and chamber[sy][sx+push] != '#' and chamber[sy+1][sx] != '#' and chamber[sy-1][sx] != '#') or 
-        (push == 1 and sx+push+3 < 7 and chamber[sy][sx+push+3] != '#' and chamber[sy-1][sx+push+2] != '#' and chamber[sy+1][sx+push+2] != '#'))):
-      pushShape(chamber, shapeID, sx,sy, push)
+        (push == -1 and chamber[sy][sx-1] != 1) or 
+        (push == 1 and sx+4 < 7 and chamber[sy][sx+4] != 1))):
       sx += push
     return sx
-  if(shapeID == 1):
-    if(0 <= sx + push< 7 and ( (push == -1 and chamber[sy][sx+push] != '#') or (push == 1 and sx+push+2 < 7 and chamber[sy][sx+push+2] != '#'))):
-      pushShape(chamber, shapeID, sx,sy, push)
+  elif(shapeID == 1):
+    if(0 <= sx + push< 7 and ( (push == -1 and chamber[sy][sx] != 1 and chamber[sy+2][sx] != 1 and chamber[sy+1][sx-1] != 1) 
+      or (push == 1 and sx+3 < 7 and chamber[sy][sx+2] != 1 and chamber[sy+2][sx+2] != 1 and chamber[sy+1][sx+3] != 1))):
       sx += push
     return sx
-  if(shapeID == 2):
+  elif(shapeID == 2):
     if(0 <= sx + push< 7 
-      and ( (push == -1 and chamber[sy][sx+push] != '#' and sx+1 < 7 and chamber[sy+1][sx+1] != '#' and chamber[sy+2][sx+1] != '#') 
-      or (push == 1 and sx+3 < 7 and chamber[sy][sx+3] != '#' and chamber[sy+1][sx+3] != '#' and chamber[sy+2][sx+3] != '#'))):
-      pushShape(chamber, shapeID, sx,sy, push)
+      and ( (push == -1 and chamber[sy][sx-1] != 1 and chamber[sy+1][sx+1] != 1 and chamber[sy+2][sx+1] != 1) 
+      or (push == 1 and sx+3 < 7 and chamber[sy][sx+3] != 1 and chamber[sy+1][sx+3] != 1 and chamber[sy+2][sx+3] != 1))):
       sx += push
     return sx
-  if(shapeID == 3):
+  elif(shapeID == 3):
     if(0 <= sx + push< 7 
-      and ( (push == -1 and sx - 1 >= 0 and chamber[sy][sx-1] != '#' and chamber[sy+1][sx-1] != '#' and chamber[sy+2][sx-1] != '#' and chamber[sy+3][sx-1] != '#') 
-      or (push == 1 and sx+1 < 7 and chamber[sy][sx+1] != '#' and chamber[sy+1][sx+1] != '#' and chamber[sy+2][sx+1] != '#' and chamber[sy+3][sx+1] != '#'))):
-      pushShape(chamber, shapeID, sx,sy, push)
+      and ( (push == -1  and chamber[sy][sx-1] != 1 and chamber[sy+1][sx-1] != 1 and chamber[sy+2][sx-1] != 1 and chamber[sy+3][sx-1] != 1) 
+      or (push == 1 and chamber[sy][sx+1] != 1 and chamber[sy+1][sx+1] != 1 and chamber[sy+2][sx+1] != 1 and chamber[sy+3][sx+1] != 1))):
       sx += push
     return sx
-  if(shapeID == 4):
+  else:
     if(0 <= sx + push< 7 
-      and ( (push == -1 and sx - 1 >= 0 and chamber[sy][sx-1] != '#' and chamber[sy+1][sx-1] != '#') 
-      or (push == 1 and sx+2 < 7 and chamber[sy][sx+2] != '#' and chamber[sy+1][sx+2] != '#'))):
-      pushShape(chamber, shapeID, sx,sy, push)
+      and ( (push == -1 and chamber[sy][sx-1] != 1 and chamber[sy+1][sx-1] != 1) 
+      or (push == 1 and sx+2 < 7 and chamber[sy][sx+2] != 1 and chamber[sy+1][sx+2] != 1))):
       sx += push
     return sx
 
@@ -140,7 +86,7 @@ def pushTing(chamber, shapeID, sx, sy, pushK = None):
 def drop(chamber,shapeID, heightToDropFrom,v = False,t = 0):
   global y
   sx,sy = 2,heightToDropFrom
-  placeShape(chamber,shapeID, sy,sx)
+  # placeShape(chamber,shapeID, sy,sx)
   if(shapeID == 0):
     while(True): 
       if(v):
@@ -148,116 +94,97 @@ def drop(chamber,shapeID, heightToDropFrom,v = False,t = 0):
         for i in chamber[::-1]:
           print("".join(i))
         print("-------")      
-      sx = pushTing(chamber, shapeID, sx,sy)
+      sx = pushShape(chamber, shapeID, sx,sy)
       placed = False
       for j in range(4):
-        if(sy - 1 < 0 or chamber[sy-1][sx+j] == "#"):
+        if(sy - 1 < 0 or chamber[sy-1][sx+j] == 1):
           placed = True
       if(placed):
         break
-      for j in range(4):
-        chamber[sy][sx+j] = "."
-        placeShape(chamber,shapeID, sy-1, sx)
         
       sy -= 1
+    placeShape(chamber,shapeID, sy, sx) 
     y = max(sy,y)
-  if(shapeID == 1):
+  elif(shapeID == 1):
     while(True):
       if(v):
         time.sleep(t)
         for i in chamber[::-1]:
           print("".join(i))
         print("-------")
-      sx = pushTing(chamber, shapeID, sx,sy+2)
+      sx = pushShape(chamber, shapeID, sx,sy)
       placed = False
-      if(sy-1 < 0 or chamber[sy-1][sx+1] == "#" or chamber[sy][sx] == "#" or chamber[sy][sx+2] == "#"):
+      if(sy-1 < 0 or chamber[sy-1][sx+1] == 1 or chamber[sy][sx] == 1 or chamber[sy][sx+2] == 1):
         placed = True
       if(placed):
         break
-      chamber[sy][sx+1] = "."
-      chamber[sy+1][sx] = "."
-      chamber[sy+1][sx+1] = "."
-      chamber[sy+1][sx+2] = "."
-      chamber[sy+2][sx+1] = "."
-      placeShape(chamber,shapeID, sy-1, sx)
       
       sy -= 1
+    placeShape(chamber,shapeID, sy, sx)
     y = max(sy + 2,y)
-  if(shapeID == 2):
+  elif(shapeID == 2):
     while(True): 
       if(v):
         time.sleep(t)
         for i in chamber[::-1]:
           print("".join(i))
         print("-------")
-      sx = pushTing(chamber, shapeID, sx,sy)
+      sx = pushShape(chamber, shapeID, sx,sy)
       placed = False
-      if(sy-1 < 0 or chamber[sy-1][sx] == "#" or chamber[sy-1][sx+1] == "#" or chamber[sy-1][sx+2] == "#"):
+      if(sy-1 < 0 or chamber[sy-1][sx] == 1 or chamber[sy-1][sx+1] == 1 or chamber[sy-1][sx+2] == 1):
         placed = True
       if(placed):
         break
-      chamber[sy][sx] = "."
-      chamber[sy][sx+1] = "."
-      chamber[sy][sx+2] = "."
-      chamber[sy+1][sx+2] = "."
-      chamber[sy+2][sx+2]= "."
-      placeShape(chamber,shapeID, sy-1, sx)
       
       sy -= 1
+    placeShape(chamber,shapeID, sy, sx)
     y = max(sy + 2,y)
-  if(shapeID == 3):
+  elif(shapeID == 3):
     while(True):
       if(v):  
         time.sleep(t)
         for i in chamber[::-1]:
           print("".join(i))
         print("-------")
-      sx = pushTing(chamber, shapeID, sx,sy)
+      sx = pushShape(chamber, shapeID, sx,sy)
       placed = False
-      if(sy-1 < 0 or chamber[sy-1][sx] == "#"):
+      if(sy-1 < 0 or chamber[sy-1][sx] == 1):
         placed = True
       if(placed):
         break
-      chamber[sy][sx] = "."
-      chamber[sy+1][sx] = "."
-      chamber[sy+2][sx] = "."
-      chamber[sy+3][sx] = "."
-      placeShape(chamber,shapeID, sy-1, sx)
       
       sy -= 1
+    placeShape(chamber,shapeID, sy, sx)
     y = max(sy + 3,y)
-  if(shapeID == 4):
+  else:
     while(True):
       if(v):  
         time.sleep(t)
         for i in chamber[::-1]:
           print("".join(i))
         print("-------")
-      sx = pushTing(chamber, shapeID, sx,sy)
+      sx = pushShape(chamber, shapeID, sx,sy)
       placed = False
-      if(sy-1 < 0 or chamber[sy-1][sx] == "#" or chamber[sy-1][sx+1] == "#"):
+      if(sy-1 < 0 or chamber[sy-1][sx] == 1 or chamber[sy-1][sx+1] == 1):
         placed = True
       if(placed):
         break
-      chamber[sy+1][sx] = "."
-      chamber[sy+1][sx+1] = "."
-      chamber[sy][sx] = "."
-      chamber[sy][sx+1] = "."
-      placeShape(chamber,shapeID, sy-1, sx)
-      
+
       sy -= 1
+    placeShape(chamber,shapeID, sy, sx)
     y = max(sy+1,y)
+  return sx,sy
     
 y = -1
-for i in range(2022):
-  drop(chamber,i%5,y+4)
-
-print(y)
-# time.sleep(1)
-# for i in chamber[::-1]:
-#   print("".join(i))
-# print("-------")
+DP = {}
+for i in range(drops):
+  a,b = drop(chamber,i%5,y+4)
   
+
+print(y+1, drops*4)
+wastage = ((toUse - y)/(toUse))
+print(wastage)
+
   
   
   
---
+++ 2022/test.py
@@ -0,0 +1,6 @@
+
+a = 0
+for i in range(1000000000000):
+  a+=1
+  
+print(a)

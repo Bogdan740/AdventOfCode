ab1297d2fafb56bb98180e9984e7bef4c886c655
Mihai-Bogdan Cuciureanu <bogdancuciureanup@gmail.com>
2023-12-08 12:37:56 +0000

    2028 D9

--- 2018/d9/p1.py
+++ 2018/d9/p1.py
@@ -3,32 +3,35 @@ with open("test.txt", "r") as fp:
     lines=fp.read()
 
 num_players, max_marble = (int(lines.split()[0]),int(lines.split()[-2]))
+print(num_players, max_marble)
 
 player = 1
 score = {}
 
-last = 0
+current = 0
 circle = [0]
 
 scores = {}
 for i in range(1,max_marble+1):
-    first_cw = (last+1) % len(circle)
-    sec_cw = (last+2) % len(circle)
+    # print(f"CURRENT : {circle[current]}")
+    # print(["_" + str(k) + "_" if k == circle[current] else k for k in circle])
+    first_cw = (current+1) % len(circle)
+    sec_cw = (current+2) % len(circle)
     if(i % 23 == 0):
+        is_mult = True
         if(player not in scores):
             scores[player] = 0
             
         scores[player]+=i
-        scores[player]+=circle[last-7]
-        circle = circle[:last-7] + circle[last-7+1:]
-        last = last-7
-        if(last==-1):
-            last=0
+        scores[player]+=circle[current-7]
+        circle = circle[:current-7] + circle[current-7+1:]
+        current = (current-7)%len(circle)
+        player=(player+1)%num_players
         continue
-        
+
     circle=circle[:first_cw+1] + [i] + circle[first_cw+1:]
-    last = first_cw+1
+    current = (first_cw+1)%len(circle)
     
-    player+=1
-        
+    player=(player+1)%num_players
+
 print(max(scores.values()))
